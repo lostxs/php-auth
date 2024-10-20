f8802c9741e4317fff011ab27adfc07c8cd17ca2
lostx <ashesdonotburn@vk.com>
2024-10-21 03:15:40 +0500

    deploy

--- env.php
+++ env.php
@@ -7,7 +7,7 @@
     'port' => 5432,
   ],
   'yandex' => [
-    'client_key' => 'ysc1_rVg3h0i9Pbr6nmpGQv1fdFS2e6WYjI9sFCNdIIKi6ba37d4c',
-    'secret_key' => 'ysc2_rVg3h0i9Pbr6nmpGQv1ftzehxNPJckWfwWENd82N24b76054',
+    'client_key' => '',
+    'secret_key' => '',
   ],
 ];
